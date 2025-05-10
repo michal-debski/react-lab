66bbad7de0baeb85dd686a314a4da0a2294cb736
mdebski <midebski@student.agh.edu.pl>
2025-05-10 10:07:53 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl'
-
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
+    let message = "";
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
+
+    if (email.length <14 && email.length >5){
+        message = "Twój adres e-mail jest w sam raz";
+    } else if (email.length >= 14) {
+        message = "Twój adres e-mail jest za długi";
+    } else {
+        message = "Ale masz krótki adres!";
+    }
+
   return (
     <div className="App">
      <h1>System do zapisywania na zajęcia</h1>
+        <h2>{message}</h2>
         <h2>Twój e-mail to {email}</h2>
-        <input type="text" onChange={handleChange}/>
+        <input type="text"  value={email} onChange={handleChange}/>
     </div>
   );
 }
