0dc00e74ee12be1eaff2d8c52454cb62cd169e39
mdebski <midebski@student.agh.edu.pl>
2025-05-11 10:26:10 +0200

    add new components

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -1,10 +1,11 @@
 import {useState} from "react";
-
+import MeetingsPage from "./meetings/MeetingsPage"
 export default function UserPanel({username, onLogout}) {
     return (
         <div>
             <h4>Witaj {username}!</h4>
             <a onClick={() => onLogout()}>Wyloguj</a>
+            <MeetingsPage/>
         </div>
     );
 }
